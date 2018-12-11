6e5550e45caf65387eaaf75ab558f9d17eb94a57
KWright16 <42691009+KWright16@users.noreply.github.com>
2018-12-11 10:34:21 +0000

    locations controller

--- controllers/trails.js
+++ controllers/trails.js
@@ -1,14 +1,4 @@
 const db = require("../firestore");
-// const admin = require("firebase-admin");
-
-// const serviceAccount = require("../treasure-hunt.json");
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://treasure-hunt-dcd8e.firebaseio.com"
-// });
-
-// const db = admin.firestore();
-// db.settings({ timestampsInSnapshots: true });
 
 exports.getTrails = (req, res, next) => {
   db.collection("trails")
