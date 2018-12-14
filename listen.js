96ceba1b53863c2469731a78733741e724155268
KWright16 <42691009+KWright16@users.noreply.github.com>
2018-12-14 15:49:26 +0000

    changed web socket

--- listen.js
+++ listen.js
@@ -13,6 +13,7 @@ websocket.on("channel1", data => {
   console.log("Greetings from RN app", data);
 });
 
-// websocket.emit('channel2', 'new channel');
-// websocket.on('channel2', (obj) => {
-//   console.log('Object from RN app', obj);
+websocket.emit("channel2", "new channel");
+websocket.on("channel2", obj => {
+  console.log("Object from RN app", obj);
+});
